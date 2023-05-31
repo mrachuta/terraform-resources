09fe3e89f5db352a7c51e7f81cf942aff06521db
mrachuta <34251394+mrachuta@users.noreply.github.com>
2023-05-31 12:05:40 +0200

    Fix issues broken outputs in loadbalancer module - string to number

--- modules/gcp-tcp-loadbalancer-module/outputs.tf
+++ modules/gcp-tcp-loadbalancer-module/outputs.tf
@@ -8,13 +8,13 @@ output "lb_ip_output" {
 
 output "lb_ip_output_clickable_http" {
   value = var.external_lb == true ? (
-    var.http_port == "80" ? (
+    var.http_port == 80 ? (
       "http://${google_compute_forwarding_rule.external_lb[0].ip_address}/"
     ) : (
       "http://${google_compute_forwarding_rule.external_lb[0].ip_address}:${var.http_port}/"
     )
   ) : (
-    var.http_port == "80" ? (
+    var.http_port == 80 ? (
       "http://${google_compute_forwarding_rule.internal_lb[0].ip_address}/"
     ) : (
       "http://${google_compute_forwarding_rule.internal_lb[0].ip_address}:${var.http_port}/"
@@ -25,7 +25,7 @@ output "lb_ip_output_clickable_http" {
 output "lb_ip_output_clickable_https" {
   value = var.external_lb == true ? (
     var.https_port != null ? (
-      var.https_port == "443" ? (
+      var.https_port == 443 ? (
         "http://${google_compute_forwarding_rule.external_lb[0].ip_address}/"
       ) : (
         "https://${google_compute_forwarding_rule.external_lb[0].ip_address}:${var.https_port}/"
@@ -33,7 +33,7 @@ output "lb_ip_output_clickable_https" {
     ) : null
   ) : (
     var.https_port != null ? (
-      var.https_port == "443" ? (
+      var.https_port == 443 ? (
         "http://${google_compute_forwarding_rule.internal_lb[0].ip_address}/"
       ) : (
         "https://${google_compute_forwarding_rule.internal_lb[0].ip_address}:${var.https_port}/"
