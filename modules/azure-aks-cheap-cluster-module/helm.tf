34e5f1b412855c47fcea08ccf829d69a88601b38
mrachuta <34251394+mrachuta@users.noreply.github.com>
2024-02-21 08:02:28 +0100

    Update host value for helm

--- modules/azure-aks-cheap-cluster-module/helm.tf
+++ modules/azure-aks-cheap-cluster-module/helm.tf
@@ -2,7 +2,7 @@
 provider "helm" {
 
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    host                   = azurerm_kubernetes_cluster.aks[0].kube_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks[0].kube_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks[0].kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks[0].kube_config[0].cluster_ca_certificate)
